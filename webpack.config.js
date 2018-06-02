b9650da9c5ed252e4445b076ebe305cab29b36d9
i-mckinney <baseballrocks@gmail.com>
2018-06-01 19:11:43 -0700

    fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,16 @@ var src = path.join(__dirname, '/client/src');
 var dist = path.join(__dirname, '/public/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: `${src}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: dist
   },
   module: {
     loaders: [
       {
         test: /\.jsx?/,
-        include: SRC_DIR,
+        include: src,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
