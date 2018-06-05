a17c200c31471b0d721b04f10a9bd20ece61b54e
i-mckinney <baseballrocks@gmail.com>
2018-06-05 16:02:00 -0700

    finalized webpack config, rendering react dom, new filepaths/components

---
+++ client/src/components/outfitGeneratorList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import outfitGeneratorListEntry from './outfitGeneratorListEntry.jsx';
+
+function OutfitGeneratorList(props) {
+  
+}
+
+export default OutfitGeneratorList;
+
