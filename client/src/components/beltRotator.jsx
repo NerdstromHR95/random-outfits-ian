4b3afeb47c6bb9ebcc35f3c48e4ad4efc82d754e
i-mckinney <baseballrocks@gmail.com>
2018-06-13 11:10:36 -0700

    final styling commit

--- client/src/components/beltRotator.jsx
+++ client/src/components/beltRotator.jsx
@@ -10,7 +10,7 @@ const BeltRotator = (props) => (
     </div>
     ) : (
     props.outfits.length === 1 ? (
-        <div className="pantslider">
+        <div className="outfitslider">
           <img id="thumbnails" src={props.outfits[0].imgUrl} alt={props.outfits[0].title} />
           <h3  style={{textAlign:"center"}}>${props.outfits[0].price}.00</h3>
         </div>
