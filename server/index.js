00569f873172fe3ac7dee2b1b249e171554f9b33
i-mckinney <baseballrocks@gmail.com>
2018-06-13 10:03:27 -0700

    removed redundant nomenclature to align with group components

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = 3003;
 
 
 
-app.get(`/gender/:gender/style/:style`, function(req, res) {
+app.get(`/random/gender/:gender/style/:style`, function(req, res) {
   const genderName = JSON.stringify(req.params.gender);
   const styleName = JSON.stringify(req.params.style);
   db.getAllProductsOfStyle(genderName, styleName, (err, products) => {
@@ -18,23 +18,22 @@ app.get(`/gender/:gender/style/:style`, function(req, res) {
       res.status(500)
       res.send();
     } else {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
       res.status(200).send(products);
-
-      // res.send(products);
     }
   })
 })
 
 
-app.get(`/products/:productId`, function(req, res) {
+app.get(`/random/products/:productId`, function(req, res) {
   const productId = req.params.productId;
   db.getProductInfo(productId, (err, info) => {
     if (err) {
       res.status(500);
       res.end();
     } else {
-      res.status(200);
-      res.send(info);
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+      res.status(200).send(info);
     }
   })
 });
